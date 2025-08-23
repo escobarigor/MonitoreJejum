c1d4c364f81af165987bd4182b488ed3658fea37
Igor Escobar <igorborbaescobar@gmail.com>
2025-08-23 16:41:41 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react( ),
+    react(  ),
     tailwindcss()
   ],
-  base: './MonitoreJejum/', // ALTERADO: Adicionado './' para caminho relativo
+  base: './', // CORRIGIDO: Alterado para caminho relativo simples
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
